737408fd0ceaadb749372ba204da38dad69e5a65
Marcin Nieborak <marcin.nieborak@gmail.com>
2019-02-10 18:33:34 +0100

    Initial configuration

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
+};
