0ff0b0f23f8e1c0e2576bbd760f72e0b37939c20
Marcin Nieborak <marcin.nieborak@gmail.com>
2019-02-10 19:07:22 +0100

    Add react

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import "./index.scss";
+import ReactDOM from "react-dom";
+import React from "react";
+
+const reactRoot = document.createElement("div");
+reactRoot.setAttribute("id", "react-root");
+document.body.appendChild(reactRoot);
+
+const App = (props) => <div><h1>Sudoku</h1></div>;
+
+ReactDOM.render(<App />, reactRoot);
 
